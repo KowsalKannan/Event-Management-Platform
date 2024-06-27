2cad642034c12e1b207be0ba77549cbf8a074804
Kowsal Kannan <kowsalkannan.m@mitrahsoft.com>
2024-06-27 21:40:49 +0530

    Set up basic structure and endpoints for Event Management Platform

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import eventRoutes from './routes/events';
+import cors from 'cors';
 
 const app = express();
 
 app.use(bodyParser.json());
-
+app.use(cors());
 app.use('/events', eventRoutes);
 
 app.use((req, res) => {
